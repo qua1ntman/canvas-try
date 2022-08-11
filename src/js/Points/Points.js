713a2d0dec5d5cdd7d743ab97e3c69c21ae907fa
Mikhail Ashla <redikfrost@gmail.com>
2022-08-11 22:29:59 +0300

    feat: add points

---
+++ src/js/Points/Points.js
@@ -0,0 +1,12 @@
+export class Points {
+  constructor(context, font) {
+    this.context = context
+    this.font = font || "48px serif"
+    this.points = 0
+  }
+
+  draw() {
+    this.context.font = this.font;
+    this.context.fillText(`${Math.round(this.points / 10)}`, 30, 50, 100);
+  }
+}
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -2,6 +2,7 @@ import platform from '../images/platform.png'
 import '../style.css'
 import { Player } from './Player/Player'
 import { Platform } from './Platform/Platform'
+import { Points } from './Points/Points';
 
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
@@ -18,15 +19,31 @@ let platforms = [
   new Platform(400, -100, canvas.height - 50, context, platformImg),
 ]
 
-for (let i=0; i<4; i++) {
-  let lastPlatform = platforms[platforms.length-1]
-  platforms = [...platforms,   new Platform(
-    Math.floor(Math.random() * 250) + 50, 
-    lastPlatform.position.x + 50 + lastPlatform.width,
-    Math.floor(Math.random() * 500) + 150, 
-    context, platformImg
-  )]  
+function addPlatforms(n) {
+  let lastPlatform
+  if (n === 1) {
+    lastPlatform= platforms[platforms.length-1]
+    platforms = [...platforms,   new Platform(
+      Math.floor(Math.random() * 250) + 50, 
+      lastPlatform.position.x + 50 + lastPlatform.width,
+      Math.floor(Math.random() * 500) + 150, 
+      context, platformImg
+    )]  
+  }
+  else {
+    for (let i=0; i<4; i++) {
+      lastPlatform= platforms[platforms.length-1]
+      platforms = [...platforms,   new Platform(
+        Math.floor(Math.random() * 250) + 50, 
+        lastPlatform.position.x + 50 + lastPlatform.width,
+        Math.floor(Math.random() * 500) + 150, 
+        context, platformImg
+      )]  
+    }
+  }
 }
+
+addPlatforms(4)
 
 const keys = {
   right: {
@@ -46,39 +63,52 @@ const keys = {
 
 let scrollOffset = 0
 
+const points = new Points(context)
+
 function animate() {
-  console.log(scrollOffset);
   setTimeout(() => {
     requestAnimationFrame(animate) // animation in browser
-  }, 1000/100);
+  }, 10);
   context.clearRect(0, 0, canvas.width, canvas.height);
 
+  // show points
+  if (points.points !== -scrollOffset) {
+    points.points = -scrollOffset
+  }
+  points.draw()
 
+  // player and platforms motion
   if (keys.right.pressed && player.position.x < 500) {
     player.velocity.x = 6
-  } else if (keys.left.pressed && player.position.x > 100) {
-    player.velocity.x = -6
+  } else if (keys.left.pressed && player.position.x >= 100) {
+    if (scrollOffset >= 0 && player.position.x === 100) {
+      console.log(scrollOffset, player.position.x, player.velocity.x, 123123);
+      player.velocity.x = 0
+    }
+    else {
+      console.log(scrollOffset, player.position.x, player.velocity.x, 33333);
+      player.velocity.x = -6
+    }
   } else {
     player.velocity.x = 0
     if (keys.left.pressed) {
-      scrollOffset += 6
-      platforms.forEach((platform) => platform.position.x += 6)
+      if (scrollOffset >= 0 && player.position.x <= 100) {
+        console.log(scrollOffset, player.position.x, player.velocity.x, 5555);
+        player.velocity.x = 0
+      } else {
+        scrollOffset += 6
+        platforms.forEach((platform) => platform.position.x += 6)
+      }
     } else if (keys.right.pressed) {
       scrollOffset -= 6
       platforms.forEach(platform => platform.position.x -= 6)
       let lastPlatform = platforms[platforms.length-1]
       if (lastPlatform.position.x + lastPlatform.width + 50 <= canvas.width ) {
-        platforms.push(
-          new Platform(
-            Math.floor(Math.random() * 250) + 50, 
-            lastPlatform.position.x + 50 + lastPlatform.width,
-            Math.floor(Math.random() * 500) + 150, 
-            context, platformImg
-          )
-        )
+        addPlatforms(1)
         if (platforms.length > 30) platforms.shift()
       }
     }
+
   }
 
   //platform vertical collision detection
@@ -101,10 +131,8 @@ function animate() {
 animate()
 
 addEventListener('keydown', ({keyCode}) => {
-  // console.log(keyCode);
   switch(keyCode) {
     case 32:
-      // console.log(keys.up, player.velocity.y);
       if (keys.up.pressed && keys.up.times > 1) return
       keys.up.pressed = true
       keys.up.times += 1
@@ -112,7 +140,6 @@ addEventListener('keydown', ({keyCode}) => {
       player.velocity.y -= 20
       break
     case 87:
-      // console.log(keys.up, player.velocity.y);
       if (keys.up.pressed && keys.up.times === 2) return
       keys.up.pressed = true
       keys.up.times += 1
@@ -120,45 +147,35 @@ addEventListener('keydown', ({keyCode}) => {
       player.velocity.y -= 20
       break
     case 83:
-      // console.log('Down');
       player.height = 15
       break
     case 65:
       keys.left.pressed = true
-      // console.log('Left');
       break
     case 68:
       keys.right.pressed = true
-      // console.log('Right');
       break
   }
-  // // console.log(keys.right.pressed);
 })
 
 addEventListener('keyup', ({keyCode}) => {
-  // // console.log(keyCode);
   switch(keyCode) {
     case 87:
-      // console.log('Up');
       player.height = 30
       break
     case 83:
-      // console.log('Down');
       player.height = 30
       player.position.y -= 15
       break
     case 65:
       keys.left.pressed = false
-      // console.log('Left');
       player.velocity.x = 0
       break
     case 68:
       keys.right.pressed = false
-      // console.log('Right');
       player.velocity.x = 0
       break
   }
-  // // console.log(keys.right.pressed);
 })
 
 addEventListener('keypress', (e) => {
